758662ad91350677682b1a7a8476a04ee467aa07
OliMoli <olimoli@disroot.org>
2026-06-28 09:49:43 +0200

    Add Nix modules and make udev rules stricter

---
+++ modules/nixos.nix
@@ -0,0 +1,53 @@
+{
+  self,
+  ...
+}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.services.bezel;
+in
+{
+  options.services.bezel = {
+    enable = lib.mkEnableOption "Enable the Bezel Trackpad Gestures service for all users";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      description = "The bezel package to use";
+    };
+
+    extraPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = with pkgs; [
+        wireplumber
+        brightnessctl
+        playerctl
+        # hyprland
+      ];
+      description = "Extra packages to add to the systemd unit's PATH. By default installs packages required by the default configuration (except for Hyprland)";
+    };
+  };
+
+  config = {
+    environment.systemPackages = lib.optional (cfg.enable && cfg.package != null) cfg.package;
+
+    systemd.user.services.bezel = {
+      description = "Bezel Trackpad Gestures";
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = lib.getExe cfg.package;
+        Restart = "always";
+        RestartSec = 3;
+      };
+      path = [ pkgs.bash ] ++ cfg.extraPackages;
+      wantedBy = [ "default.target" ];
+    };
+
+    hardware.uinput.enable = true;
+  };
+}
